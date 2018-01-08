faeab37c5c4b891ef9327f67ac536acf536d02ca
FHGDev <gideon.lingle14@gmail.com>
2018-01-08 16:56:01 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,20 +4,29 @@ const Enmap = require('enmap')
 const EnmapLevel = require('enmap-level')
 bot.login(process.env.token);
 
-const serverprovider = new EnmapLevel({name: 'servers'});
-this.servers = new Enmap({provider: servers})
+bot.settings = new Enmap({name: 'settings', persistent: true})
 
-bot.serverMonitor()
+bot.settings.set("filter" defaultSettings.filter)
+
+const defaultSettings = {
+  prefix: ";",
+  modLogChannel: "bot-audit",
+  premiumrole: "SuperPatron",
+  adminRole: "Admin",
+  welcome: "Welcome to {server!}",
+  filter: true
+}
 
 
 
-bot.on("ready" () => require('./events/ready.js')(bot))
+
+
+bot.on(`ready` () => require('./events/ready.js')(bot, defaultSettings))
 bot.on(`error`, (error) => console.log(error));
 bot.on(`disconnect`, () => console.log(`Client connection attempts: FAILED`));
-bot.on(`message`, (msg) => require("./events/message.js")(bot, msg, bot.commands));
-bot.on(`message`, (msg) => require("./musicbot/musicHandle.js")(bot, msg));
-bot.on(`guildMemberAdd`, (member) => require("./events/guildMemberAdd.js")(bot, member));
-bot.on(`guildMemberRemove`, (member) => require("./events/guildMemberRemove.js")(bot, member));
-bot.on(`guildBanRemove`, (guild, member) => require("./events/guildBanRemove.js")(bot, guild, member));
-bot.on(`guildCreate`, (guild) => require("./events/guildCreate.js")(bot, guild));
-bot.on(`guildDelete`, (guild) => require("./events/guildDelete.js")(bot, guild));
+bot.on(`message`, (msg) => require("./events/message.js")(bot, msg, bot.commands, defaultSettings));
+bot.on(`guildMemberAdd`, (member) => require("./events/guildMemberAdd.js")(bot, member, defaultSettings));
+bot.on(`guildMemberRemove`, (member) => require("./events/guildMemberRemove.js")(bot, member defaultSettings));
+bot.on(`guildBanRemove`, (guild, member) => require("./events/guildBanRemove.js")(bot, guild, member, defaultSettings));
+bot.on(`guildCreate`, (guild) => require("./events/guildCreate.js")(bot, guild, defaultSettings));
+bot.on(`guildDelete`, (guild) => require("./events/guildDelete.js")(bot, guild, defaultSettings));

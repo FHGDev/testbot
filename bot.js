898a70544bc77b9a0fe7a0eb52e600df8d41877c
FHGDev <gideon.lingle14@gmail.com>
2018-01-08 17:27:01 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,32 +1,23 @@
 const discord = require('discord.js');
 const bot = new discord.Client();
 const Enmap = require('enmap')
-const EnmapLevel = require('enmap-level')
+const Rethink = require('enmap-rethink')
+const provider = new Rethink({name: "settings"})
 bot.login(process.env.token);
-
-bot.settings = new Enmap({name: 'settings', persistent: true})
-
-bot.settings.set("filter" defaultSettings.filter)
+bot.settings = new Enmap({name: 'settings', persistent: true});
 
 const defaultSettings = {
-  prefix: ";",
-  modLogChannel: "bot-audit",
-  premiumrole: "SuperPatron",
+  prefix: ":",
+  modLogChannel: "mod-log",
+  filter: true,
   adminRole: "Admin",
-  welcome: "Welcome to {server!}",
-  filter: true
+  welcomeMessage: "Say hello to {{user}}, everyone! We all need a warm welcome sometimes :D"
 }
 
+const gsettings {
+  
+}
 
-
-
-
-bot.on(`ready` () => require('./events/ready.js')(bot, defaultSettings))
-bot.on(`error`, (error) => console.log(error));
-bot.on(`disconnect`, () => console.log(`Client connection attempts: FAILED`));
-bot.on(`message`, (msg) => require("./events/message.js")(bot, msg, bot.commands, defaultSettings));
-bot.on(`guildMemberAdd`, (member) => require("./events/guildMemberAdd.js")(bot, member, defaultSettings));
-bot.on(`guildMemberRemove`, (member) => require("./events/guildMemberRemove.js")(bot, member defaultSettings));
-bot.on(`guildBanRemove`, (guild, member) => require("./events/guildBanRemove.js")(bot, guild, member, defaultSettings));
-bot.on(`guildCreate`, (guild) => require("./events/guildCreate.js")(bot, guild, defaultSettings));
-bot.on(`guildDelete`, (guild) => require("./events/guildDelete.js")(bot, guild, defaultSettings));
+bot.on("guildCreate", guild => {
+  bot.settings.set(guild.id, defaultSettings);
+});

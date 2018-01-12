ecd49a55323e522954f148001e94ab048ca5aba8
FHGDev <gideon.lingle14@gmail.com>
2018-01-11 21:59:34 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,23 +1,36 @@
 const discord = require('discord.js');
 const bot = new discord.Client();
 const Enmap = require('enmap')
-const Rethink = require('enmap-rethink')
-const provider = new Rethink({name: "settings"})
+const EnmapLevel = require('enmap-level')
+const pak = require('./package.json')
 bot.login(process.env.token);
 bot.settings = new Enmap({name: 'settings', persistent: true});
 
 const defaultSettings = {
-  prefix: ":",
-  modLogChannel: "mod-log",
+  prefix: "t!",
+  modLogChannel: "bot-audit",
   filter: true,
   adminRole: "Admin",
   welcomeMessage: "Say hello to {{user}}, everyone! We all need a warm welcome sometimes :D"
 }
 
-const gsettings {
-  
-}
+bot.on("ready", () => {
+	bot.user.setGame(`t!help | ${bot.guilds.array().length} servers`)
+	console.log(`${bot.user.username} v${pak.version} is ready!`)
+	bot.guilds.forEach((guild, id => {
+		console.log(`[SERVER] [${guild.memberCount}] ${guild.name} ($guild.id}) | Joined: ${guild.joinedAt.toString()}
+	})
+});
 
 bot.on("guildCreate", guild => {
   bot.settings.set(guild.id, defaultSettings);
 });
+
+bot.on("guildDelete", guild => {
+	bot.settings.delete(guild.id)
+})
+
+bot.on("guildMemberAdd", member => {
+	defaultSettings.welcomeMessage.replace("{{user}}", member.displayName)
+})
+
